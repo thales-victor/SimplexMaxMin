cc9041e4ce3c4cb225e123e6c9ae47176b46695e
Thales Cardoso <thales.cardoso@meep-app.com>
2019-10-24 18:17:05 -0300

    Add project files.

---
+++ SimplexMaxMin/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SimplexMaxMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        private static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Pressione F1 para inserir a coluna Z");
+            Console.WriteLine("Pressione F2 para inserir as demais colunas");
+            Console.WriteLine("Pressione F3 para solucionar o Simplex passo-a-passo");
+            Console.WriteLine("Pressione F5 para solucionar o Simplex até o final");
+
+            try
+            {
+                var cki = Console.ReadKey();
+
+                switch (cki.Key)
+                {
+                    case ConsoleKey.F1:
+                        {
+                            InserirColunaZ();
+                        }
+                        break;
+                }
+            }
+            catch
+            {
+                Menu();
+            }
+        }
+
+        private static void InserirColunaZ ()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Inserindo coluna Z");
+
+            Console.ReadKey();
+
+            Menu();
+        }
+    }
+}
